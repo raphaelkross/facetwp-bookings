9978b83107f6151c02f50c7a79d3c5a58653012c
mgibbs189 <mgibbs189@gmail.com>
2016-06-15 14:01:16 -0400

    Automatic updates

--- facetwp-bookings.php
+++ facetwp-bookings.php
@@ -3,29 +3,14 @@
 Plugin Name: FacetWP - Bookings Integration
 Plugin URI: https://facetwp.com/
 Description: WooCommerce Bookings support
-Version: 0.2
-Author: Matt Gibbs
-GitHub Plugin URI: https://github.com/FacetWP/facetwp-bookings
-
-Copyright 2016 Matt Gibbs
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, see <http://www.gnu.org/licenses/>.
+Version: 0.3
+Author: FacetWP, LLC
+GitHub URI: facetwp/facetwp-bookings
 */
 
 defined( 'ABSPATH' ) or exit;
 
-include( dirname( __FILE__ ) . '/install-github-updater.php' );
+include( dirname( __FILE__ ) . '/github-updater.php' );
 
 /**
  * Register facet type
